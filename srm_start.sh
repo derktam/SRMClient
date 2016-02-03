2a15860a71a623eacf80b7e740c92501cb90298d
root <root@comtros.(none)>
2016-02-03 14:11:26 +0900

    srm_stat 스크립트 추가

---
+++ srm_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function start(){
+    is_run=$(forever list | wc -l)
+    if [ $is_run = 1 ]; then
+        echo "실행 중이 아님!"
+        forever start /root/SRMClient/bin/main.js
+    fi
+}
+
+
+start
