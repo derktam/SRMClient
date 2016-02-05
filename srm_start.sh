b951c20e1eadf206d40c7411ec596253523698a2
root <root@comtros.(none)>
2016-02-05 10:06:44 +0900

    srm_start 수정

--- srm_start.sh
+++ srm_start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-function start(){
+function check_start(){
     is_run=$(forever list | wc -l)
-    if [ $is_run = 1 ]; then
+    if [ "$is_run" == "1" ]; then
         echo "실행 중이 아님!"
         forever start /root/SRMClient/bin/main.js
     fi
 }
 
 
-start
+check_start
